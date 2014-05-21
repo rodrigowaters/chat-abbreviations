13e3d2a6ab9579577a53933a08e26fe1c0ded07e
Rodrigo Silva <rodrigo.silva@solucoesglobais.com.br>
2014-05-21 13:19:55 -0300

    Novo layout

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-var select;
+var list_language;
 function onLocalized() {
     var l10n = document.webL10n;
-    select = document.getElementsByTagName('select')[0];
-    select.onchange = function() {
-        l10n.setLanguage(this.value || this.options[this.selectedIndex].text);
-    };
+    list_language = document.querySelectorAll('#index nav ul li a');
+    var total_language = list_language.length;
+    for (var i = 0; i < total_language; i++) {
+        list_language.item(i).onclick = function() {
+            var language = this.id;
+            var language_text = this.text;
+            l10n.setLanguage(language || language_text);
+        };
+    }
 }
 document.webL10n.ready(onLocalized);
-setTimeout(function(){
-        select.onchange();
-},500);
+setTimeout(function() {
+    list_language.item(0).click();
+}, 500);
