a98267709a3bb0e999592b640a69dfb6988df67b
Rodrigo Waters <rdrgwtrs@gmail.com>
2014-06-29 20:34:31 -0300

    Release version 2.0
    new layout

--- js/app.js
+++ js/app.js
@@ -1,28 +1,31 @@
 var list_language;
 function onLocalized() {
     var l10n = document.webL10n;
-    list_language = document.querySelectorAll('#index nav ul li a');
+    list_language = document.querySelectorAll('main#main menu div.list div.item');
     var total_language = list_language.length;
     for (var i = 0; i < total_language; i++) {
         list_language.item(i).onclick = function() {
-            var language = this.id;
-            var language_text = this.text;
-            l10n.setLanguage(language || language_text);
+            var language = $(this).data('l10n');
+            l10n.setLanguage(language);
+            $('main#main section header.bar-header button').click();
         };
     }
 }
 $(document).ready(function() {
     document.webL10n.ready(onLocalized);
-    $('#index #drawer form input').keyup(function() {
+    $('main#main section header.bar-subheader div.bar-header input').keyup(function() {
         var text = $.trim($(this).val());
         if (text) {
-            $('#index #drawer article ul li').hide();
-            $("#index #drawer article ul li a p:first-child:contains('" + (text.toUpperCase()) + "')").closest('li').fadeIn();
+            $('main#main section article ul li').hide();
+            $("main#main section article ul li h2:contains('" + (text.toUpperCase()) + "')").closest('li').fadeIn();
         } else {
-            $('#index #drawer article ul li').show();
+            $('main#main section article ul li').show();
         }
     });
-    $('#index #drawer form button').click(function() {
-        $('#index #drawer article ul li').show();
+    $('main#main section header.bar-subheader div.bar-header button').click(function() {
+        $('main#main section header.bar-subheader div.bar-header input').val('').keyup();
+    });
+    $('main#main section header.bar-header button').click(function() {
+        $('main#main').toggleClass('show_menu');
     });
 });
